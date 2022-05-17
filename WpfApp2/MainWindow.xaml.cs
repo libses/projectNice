52bcbe72513b3017eb7b3642d7f3fcadc4518061
Nazar Garipov <nazar.garipov475@gmail.com>
2022-05-17 20:46:21 +0500

    Add simple wpf ui

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // btn.Click += (sender, args) => btn.Content = "Clicked";
+            MePlayer.Source = new Uri("https://stsci-opo.org/STScI-01G0MSZ1K4MDRWH3ATFGNPBYB2.mp4");
+            PauseBtn.Click += (_, _) => MePlayer.Pause();
+            PlayBtn.Click += (_, _) => MePlayer.Play();
+        }
+    }
+}
