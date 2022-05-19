f50450edd82794a47caf04e5d7acd04d4f0baafa
Nazar Garipov <nazar.garipov475@gmail.com>
2022-05-19 21:22:56 +0500

    Add music

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -4,9 +4,11 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Windows;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using Domain;
 using Domain.Render;
+using Microsoft.Win32;
 using Image = System.Windows.Controls.Image;
 
 namespace WpfApp2
@@ -19,45 +21,44 @@ namespace WpfApp2
         public MainWindow()
         {
             InitializeComponent();
-
-            //  MePlayer.Source = new Uri("https://stsci-opo.org/STScI-01G0MSZ1K4MDRWH3ATFGNPBYB2.mp4");
-            // PauseBtn.Click += (_, _) => MePlayer.Pause();
-            // PlayBtn.Click += (_, _) => MePlayer.Play();
-            //     var img = new Image(){Width = 100, Height = 200};
             var i = 1d;
-            var bmp = ImageBase.Create()
-                .Config(new ImageSettings(1500, 1500))
-                .Add<Mandelbrot>(m => m.Config(new MandelbrotSettings(i, 0.311, 0.482)))
-                .Multiply<Gradient>(g => g)
-                .GetBitmap();
-
-            ImageViewer1.Source = BitmapToImageSource(bmp);
-            PlayBtn.Click += (_, _) =>
-            {
-                i /= 1.05;
-                var bmp = ImageBase.Create()
+            var bmp =
+                ImageBase.Create()
                     .Config(new ImageSettings(1500, 1500))
                     .Add<Mandelbrot>(m => m.Config(new MandelbrotSettings(i, 0.311, 0.482)))
                     .Multiply<Gradient>(g => g)
                     .GetBitmap();
-                ImageViewer1.Source = BitmapToImageSource(bmp);
-            };
+
+            ImageViewer1.Source = BitmapToImageSource(bmp);
+            PlayBtn.Click += (_, _) => mediaPlayer.Play();
+            PauseBtn.Click += (_, _) => mediaPlayer.Pause();
         }
 
         static BitmapImage BitmapToImageSource(DirectBitmap bitmap)
         {
-            using (MemoryStream memory = new MemoryStream())
-            {
-                bitmap.Bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
-                memory.Position = 0;
-                BitmapImage bitmapimage = new BitmapImage();
-                bitmapimage.BeginInit();
-                bitmapimage.StreamSource = memory;
-                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
-                bitmapimage.EndInit();
+            using MemoryStream memory = new MemoryStream();
+            bitmap.Bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+            memory.Position = 0;
+            BitmapImage bitmapimage = new BitmapImage();
+            bitmapimage.BeginInit();
+            bitmapimage.StreamSource = memory;
+            bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+            bitmapimage.EndInit();
 
-                return bitmapimage;
+            return bitmapimage;
+        }
+
+        private MediaPlayer mediaPlayer = new MediaPlayer();
+
+        private void btnOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Audio files (*.mp3;*.wav;*.aac)|*.mp3;*.wav;*.aac";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                mediaPlayer.Open(new Uri(openFileDialog.FileName));
             }
+            
         }
     }
 }
