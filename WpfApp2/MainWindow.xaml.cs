ca9e1c1000ccd7e6ec8a9ee5656a0ebd8a96be98
Nazar Garipov <nazar.garipov475@gmail.com>
2022-05-19 13:14:10 +0500

    Add images from Domain

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -1,5 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
 using System.Windows;
+using System.Windows.Media.Imaging;
+using Domain;
+using Domain.Render;
+using Image = System.Windows.Controls.Image;
 
 namespace WpfApp2
 {
@@ -11,10 +19,45 @@ namespace WpfApp2
         public MainWindow()
         {
             InitializeComponent();
-            // btn.Click += (sender, args) => btn.Content = "Clicked";
-            MePlayer.Source = new Uri("https://stsci-opo.org/STScI-01G0MSZ1K4MDRWH3ATFGNPBYB2.mp4");
-            PauseBtn.Click += (_, _) => MePlayer.Pause();
-            PlayBtn.Click += (_, _) => MePlayer.Play();
+
+            //  MePlayer.Source = new Uri("https://stsci-opo.org/STScI-01G0MSZ1K4MDRWH3ATFGNPBYB2.mp4");
+            // PauseBtn.Click += (_, _) => MePlayer.Pause();
+            // PlayBtn.Click += (_, _) => MePlayer.Play();
+            //     var img = new Image(){Width = 100, Height = 200};
+            var i = 1d;
+            var bmp = ImageBase.Create()
+                .Config(new ImageSettings(1500, 1500))
+                .Add<Mandelbrot>(m => m.Config(new MandelbrotSettings(i, 0.311, 0.482)))
+                .Multiply<Gradient>(g => g)
+                .GetBitmap();
+
+            ImageViewer1.Source = BitmapToImageSource(bmp);
+            PlayBtn.Click += (_, _) =>
+            {
+                i /= 1.05;
+                var bmp = ImageBase.Create()
+                    .Config(new ImageSettings(1500, 1500))
+                    .Add<Mandelbrot>(m => m.Config(new MandelbrotSettings(i, 0.311, 0.482)))
+                    .Multiply<Gradient>(g => g)
+                    .GetBitmap();
+                ImageViewer1.Source = BitmapToImageSource(bmp);
+            };
+        }
+
+        static BitmapImage BitmapToImageSource(DirectBitmap bitmap)
+        {
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                BitmapImage bitmapimage = new BitmapImage();
+                bitmapimage.BeginInit();
+                bitmapimage.StreamSource = memory;
+                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapimage.EndInit();
+
+                return bitmapimage;
+            }
         }
     }
 }
