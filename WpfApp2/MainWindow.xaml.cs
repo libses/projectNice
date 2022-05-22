f1be84666d1cdf892c0c0f079d18a0fea856103b
Nazar Garipov <nazar.garipov475@gmail.com>
2022-05-22 15:52:58 +0500

    Refactor

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -1,18 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Drawing.Imaging;
-using System.IO;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using Domain;
-using Domain.Render;
 using Microsoft.Win32;
-using NAudio.Wave;
-using Image = System.Windows.Controls.Image;
 
 namespace WpfApp2
 {
@@ -28,8 +20,7 @@ namespace WpfApp2
     /// </summary>
     public partial class MainWindow : Window
     {
-        private Image imgViewer;
-        private MediaPlayer mediaPlayer = new();
+        private readonly MediaPlayer mediaPlayer = new();
 
 
         public MainWindow()
@@ -37,11 +28,12 @@ namespace WpfApp2
             InitializeComponent();
             PlayBtn.Click += (_, _) =>
             {
-                if (videoInitialized)
+                while (!videoInitialized)
                 {
-                    isStarted = true;
-                    mediaPlayer.Play();
                 }
+
+                isStarted = true;
+                mediaPlayer.Play();
             };
             PauseBtn.Click += (_, _) =>
             {
@@ -53,7 +45,7 @@ namespace WpfApp2
         private static bool isStarted;
         private static bool videoInitialized;
 
-        void StartImageUpdater(string path)
+        private void StartImageUpdater(string path, CancellationToken ct)
         {
             var generator = new VideoGenerator(path, 1280, 720);
             var i = 0;
@@ -62,6 +54,8 @@ namespace WpfApp2
                 videoInitialized = true;
                 var temp = i;
                 i++;
+                if (ct.IsCancellationRequested)
+                    break;
                 while (!isStarted)
                 {
                 }
@@ -76,14 +70,21 @@ namespace WpfApp2
             }
         }
 
+        private CancellationTokenSource cts = new();
+
         private void btnOpenFile_Click(object sender, RoutedEventArgs e)
         {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = "Audio files (*.mp3;*.wav;*.aac)|*.mp3;*.wav;*.aac";
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = "Audio files (*.mp3;*.wav;*.aac)|*.mp3;*.wav;*.aac"
+            };
+            cts.Cancel();
+            cts = new CancellationTokenSource();
             if (openFileDialog.ShowDialog() == true)
             {
                 mediaPlayer.Open(new Uri(openFileDialog.FileName));
-                Task.Run(() => StartImageUpdater(openFileDialog.FileName));
+                isStarted = false;
+                Task.Run(() => StartImageUpdater(openFileDialog.FileName, cts.Token));
             }
         }
     }
--- WpfApp2/VideoGenerator.cs
+++ WpfApp2/VideoGenerator.cs
@@ -1,16 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Drawing;
-using System.Drawing.Imaging;
 using System.Linq;
-using System.Threading.Tasks;
-using Domain;
-using Domain.Render;
-using Domain.Settings;
-using FFMediaToolkit;
-using FFMediaToolkit.Encoding;
-using FFMediaToolkit.Graphics;
-using ILGPU;
+using Kernel.Domain.Settings;
+using Kernel.Domain;
 using NAudio.Wave;
 using Spectrogram;
 
@@ -18,12 +10,11 @@ namespace WpfApp2;
 
 public class VideoGenerator
 {
-    private string filePath;
+    private readonly string filePath;
     private readonly int width;
     private readonly int height;
 
-    private static readonly string tempFilesPath =
-        $@"C:\Users\Garipov\RiderProjects\ProjectNice\WpfApp2\bin\Debug\net6.0-windows\temp_img";
+    private const string TempFilesPath = $@"C:\Users\Garipov\RiderProjects\ProjectNice\WpfApp2\bin\Debug\net6.0-windows\temp_img";
 
     public VideoGenerator(string path, int width, int height)
     {
@@ -32,24 +23,24 @@ public class VideoGenerator
         this.height = height;
     }
 
-    public (double[] audio, int sampleRate) ReadWavMono(double multiplier = 16_000)
+    private (double[] audio, int sampleRate) ReadWavMono(double multiplier = 16_000)
     {
         using var afr = new AudioFileReader(filePath);
-        int sampleRate = afr.WaveFormat.SampleRate;
-        int bytesPerSample = afr.WaveFormat.BitsPerSample / 8;
-        int sampleCount = (int)(afr.Length / bytesPerSample);
-        int channelCount = afr.WaveFormat.Channels;
+        var sampleRate = afr.WaveFormat.SampleRate;
+        var bytesPerSample = afr.WaveFormat.BitsPerSample / 8;
+        var sampleCount = (int)(afr.Length / bytesPerSample);
+        var channelCount = afr.WaveFormat.Channels;
         var audio = new List<double>(sampleCount);
         var buffer = new float[sampleRate * channelCount];
-        int samplesRead = 0;
+        int samplesRead;
         while ((samplesRead = afr.Read(buffer, 0, buffer.Length)) > 0)
             audio.AddRange(buffer.Take(samplesRead).Select(x => x * multiplier));
         return (audio.ToArray(), sampleRate);
     }
 
-    public List<double[]> GenerateMusicVideoFull()
+    private List<double[]> GenerateMusicVideoFull()
     {
-        (double[] audio, int sampleRate) = ReadWavMono();
+        var (audio, sampleRate) = ReadWavMono();
         var sg = new SpectrogramGenerator(sampleRate, fftSize: 4096, stepSize: 2000, maxFreq: 3000);
         sg.Add(audio);
         var fft = sg.GetFFTs();
@@ -60,13 +51,12 @@ public class VideoGenerator
 
     public IEnumerable<int> Planets(int speed)
     {
-      //  var fft = GenerateMusicVideoFull();
         var f = new Planets(width, height).Config(new PlanetsSettings(20, 10, 100, Brushes.Chartreuse));
         f.speed = 10;
-        for (int i = 0; i < 10000000; i++)
+        for (var i = 0; i < 10000000; i++)
         {
             var bmp = f.GetBitmap().Bitmap;
-            bmp.Save($@"{tempFilesPath}\{i}.bmp");
+            bmp.Save($@"{TempFilesPath}\{i}.bmp");
             bmp.Dispose();
             yield return 1;
         }
@@ -78,7 +68,7 @@ public class VideoGenerator
         for (int i = 0; i < fft.Count; i++)
         {
             var bmp = f.GetBitmap().Bitmap;
-            bmp.Save($@"{tempFilesPath}\{i}.bmp");
+            bmp.Save($@"{TempFilesPath}\{i}.bmp");
             bmp.Dispose();
             yield return 1;
         }
